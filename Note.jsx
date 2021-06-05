d77ab968e8556054cf5d1748f55c79cd5f7a4899
Hemang Awasthi <82763976+hemangawasthi@users.noreply.github.com>
2021-06-05 18:06:17 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>
+        Attended a 7 day bootcamp on Javascript and React.js
+      </p>
+    </div>
+  );
+}
+
+export default Note;
